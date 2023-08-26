7f4cfb9ebb127aeeb87dd5e7b7c2ea6f019fc609
berteek <merzinalbert@gmail.com>
2023-08-26 22:33:31 +0400

    feat(main.go): setup basic gin application

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "log"
+import (
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+)
 
 func main() {
-    log.Println("hello, world!")
+    r := gin.Default()
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "pong!!!",
+        })
+    })
+    r.Run()
 }
