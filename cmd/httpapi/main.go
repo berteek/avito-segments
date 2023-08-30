27c3d811253f63fa6b04b54c929a3b4878256396
berteek <merzinalbert@gmail.com>
2023-08-30 23:12:01 +0400

    feat(main): inject service and repository into the engine

--- cmd/httpapi/main.go
+++ cmd/httpapi/main.go
@@ -4,10 +4,14 @@ import (
     "log"
 
     "github.com/berteek/avito-segments/internal/api"
+    svc  "github.com/berteek/avito-segments/internal/services"
+    rep "github.com/berteek/avito-segments/internal/repository"
 )
 
 func main() {
-    e := api.MakeEngine()
+    r := rep.NewMockSegmentRepository()
+    s := svc.NewSegmentService(r)
+    e := api.MakeEngine(s)
     err := e.Run()
     if err != nil {
         log.Fatalf("could not run the engine: %v", err)
