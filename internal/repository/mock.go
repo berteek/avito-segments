5c36259555c8d73624b8010ec7b9e10b5b06aa0b
berteek <merzinalbert@gmail.com>
2023-08-31 14:40:11 +0400

    feat(repository): add methods for segments in mock

--- internal/repository/mock.go
+++ internal/repository/mock.go
@@ -20,3 +20,14 @@ func (r MockSegmentRepository) DeleteSegment(ctx context.Context, slug string) e
     log.Printf("[repository] segment deleted: %s", slug)
     return nil
 }
+
+func (r MockSegmentRepository) GetActiveSegmentsForUser(ctx context.Context, userID int) ([]string, error) {
+    log.Printf("[repository] looking for active segments for user (id=%d)", userID)
+    segments := make([]string, 0)
+    return segments, nil
+}
+
+func (r MockSegmentRepository) AddAndDeleteSegmentsFromUser(ctx context.Context, userID int, segAdd []string, segDel []string) error {
+    log.Printf("[repository] adding and deleting segments for user (id=%d): add=%v, delete=%v", userID, segAdd, segDel)
+    return nil
+}
